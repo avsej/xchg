5a5cfac4ff7aebcb9fdd303192014ff357606170
Sergey Avseyev <sergey.avseyev@gmail.com>
2011-01-07 00:43:45 +0200

    group items by date and display meta info

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,7 +2,7 @@ class ItemsController < ApplicationController
   respond_to :html
 
   def index
-    @items = current_user.items rescue []
+    @items = current_user.items.group_by{|i| i.attachment_updated_at.to_date} rescue []
     respond_with @items
   end
 
