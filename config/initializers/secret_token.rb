694490cf0f1adeef4cf905cf293872077f22fa00
Sergey Avseyev <sergey.avseyev@gmail.com>
2011-01-06 20:58:24 +0200

    generate rails skeleton

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Xchg::Application.config.secret_token = 'ccc03ff750a8193293577d7377f4c0f063bed45826315244a2038d30ce9c08a24951bf0b98653bbdb8e55ba689003fec00a70b84290dc7716ebc538eb0d96140'
