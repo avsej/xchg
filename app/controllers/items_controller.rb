3696420a8cf36ee7d0103a7d17475f14565dde28
Sergey Avseyev <sergey.avseyev@gmail.com>
2011-01-07 00:55:20 +0200

    implement file upload

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,8 +2,18 @@ class ItemsController < ApplicationController
   respond_to :html
 
   def index
-    @items = current_user.items.group_by{|i| i.attachment_updated_at.to_date} rescue []
-    respond_with @items
+    if current_user
+      @items = current_user.items.group_by{|i| i.attachment_updated_at.to_date}
+      @item = current_user.items.build
+    else
+      @items = {}
+    end
+      respond_with(@items)
+  end
+
+  def create
+    @item = current_user.items.create(params[:item])
+    redirect_to items_path
   end
 
 end
