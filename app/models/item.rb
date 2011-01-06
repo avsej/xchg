548d67b00d995a31f50100f010d17068a4805245
Sergey Avseyev <sergey.avseyev@gmail.com>
2011-01-06 22:03:43 +0200

    use CNAME for amazon S3

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,9 +1,11 @@
 class Item < ActiveRecord::Base
-  has_attached_file :attachment, :storage => :s3, :path => "xchg/:sha1/:filename",
-                    :s3_credentials => {:bucket => ENV['S3_BUCKET'],
-                                        :access_key_id => ENV['S3_ID'],
-                                        :secret_access_key => ENV['S3_KEY']}
   validates_attachment_presence :attachment
+  has_attached_file :attachment, :storage => :s3,
+                    :url => ":s3_alias_url", :path => "/xchg/:sha1/:filename",
+                    :bucket => ENV['S3_BUCKET'],
+                    :s3_headers => { 'Expires' => 1.year.from_now.httpdate },
+                    :s3_host_alias => ENV['S3_CNAME'],
+                    :s3_credentials => {:access_key_id => ENV['S3_ID'], :secret_access_key => ENV['S3_KEY']}
 
   before_create :generate_sha1_digest
 
